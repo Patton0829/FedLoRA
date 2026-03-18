050c45789f5c9114ea6d364664ab7f45b621f737
Patton0829 <patton0829@gmail.com>
2026-03-18 11:56:50 +0800

    修改推理脚本，适配Qwen

--- GlobalModel_generated.py
+++ GlobalModel_generated.py
@@ -1,22 +1,19 @@
+import json
 import os
+
+os.environ["NO_PROXY"] = "127.0.0.1,localhost"
+os.environ["no_proxy"] = "127.0.0.1,localhost"
 
 import fire
 import gradio as gr
 import torch
 import transformers
 
-from peft import (
-    PeftModel,
-    LoraConfig,
-    get_peft_model,
-    get_peft_model_state_dict,
-    prepare_model_for_int8_training,
-    set_peft_model_state_dict,
-)
-
-from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer,AutoTokenizer
+from peft import LoraConfig, PeftModel, set_peft_model_state_dict
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 from utils.callbacks import Iteratorize, Stream
 from utils.prompter import Prompter
+
 if torch.cuda.is_available():
     device = "cuda"
 else:
@@ -25,15 +22,46 @@ else:
 try:
     if torch.backends.mps.is_available():
         device = "mps"
-except:
+except Exception:
     pass
+
+
+def _load_hf_token():
+    token_path = "./HF_key.json"
+    if not os.path.exists(token_path):
+        return None
+
+    with open(token_path, "r", encoding="utf-8") as file:
+        keys = json.load(file)
+
+    return keys.get("hf_token")
+
+
+def _build_model_kwargs(load_8bit):
+    hf_token = _load_hf_token()
+    kwargs = {}
+    if hf_token:
+        kwargs["token"] = hf_token
+
+    if device == "cuda":
+        kwargs["device_map"] = "auto"
+        kwargs["dtype"] = torch.float16
+    elif device == "mps":
+        kwargs["device_map"] = {"": device}
+        kwargs["dtype"] = torch.float16
+    else:
+        kwargs["device_map"] = {"": device}
+        kwargs["low_cpu_mem_usage"] = True
+        kwargs["dtype"] = torch.float32
+
+    return kwargs
 
 
 def main(
     load_8bit: bool = False,
     base_model: str = "",
     lora_weights_path: str = "",
-    lora_config_path: str= "", # provide only the file path, excluding the file name 'adapter_config.json'
+    lora_config_path: str = "",  # provide only the file path, excluding the file name 'adapter_config.json'
     prompt_template: str = "",  # The prompt template to use, will default to alpaca.
     server_name: str = "127.0.0.1",
     share_gradio: bool = False,
@@ -44,63 +72,50 @@ def main(
     ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
 
     prompter = Prompter(prompt_template)
-    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    tokenizer_kwargs = {}
+    hf_token = _load_hf_token()
+    if hf_token:
+        tokenizer_kwargs["token"] = hf_token
+
+    tokenizer = AutoTokenizer.from_pretrained(base_model, **tokenizer_kwargs)
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id or 0
+    tokenizer.padding_side = "left"
+
+    model_kwargs = _build_model_kwargs(load_8bit)
+    if load_8bit:
+        print("Warning: current local transformers/Qwen setup does not support `load_in_8bit` here. Falling back to non-8bit loading.")
     if not lora_weights_path.endswith(".bin"):
+        model = AutoModelForCausalLM.from_pretrained(base_model, **model_kwargs)
+        peft_kwargs = {}
+        if hf_token:
+            peft_kwargs["token"] = hf_token
         if device == "cuda":
-            model = LlamaForCausalLM.from_pretrained(
-                base_model,
-                load_in_8bit=load_8bit,
-                torch_dtype=torch.float16,
-                device_map="auto",
-            )
-            model = PeftModel.from_pretrained(
-                model,
-                lora_weights_path,
-                torch_dtype=torch.float16,
-            )
+            peft_kwargs["dtype"] = torch.float16
         elif device == "mps":
-            model = LlamaForCausalLM.from_pretrained(
-                base_model,
-                device_map={"": device},
-                torch_dtype=torch.float16,
-            )
-            model = PeftModel.from_pretrained(
-                model,
-                lora_weights_path,
-                device_map={"": device},
-                torch_dtype=torch.float16,
-            )
+            peft_kwargs["device_map"] = {"": device}
+            peft_kwargs["dtype"] = torch.float16
         else:
-            model = LlamaForCausalLM.from_pretrained(
-                base_model, device_map={"": device}, low_cpu_mem_usage=True
-            )
-            model = PeftModel.from_pretrained(
-                model,
-                lora_weights_path,
-                device_map={"": device},
-            )
+            peft_kwargs["device_map"] = {"": device}
+
+        model = PeftModel.from_pretrained(model, lora_weights_path, **peft_kwargs)
     else:
-        model = LlamaForCausalLM.from_pretrained(
-            base_model,
-            load_in_8bit=True,
-            torch_dtype=torch.float16,
-            device_map="auto",
-        )
-        model = prepare_model_for_int8_training(model)
+        assert lora_config_path, "Please specify --lora_config_path when --lora_weights_path is a .bin file"
+        model = AutoModelForCausalLM.from_pretrained(base_model, **model_kwargs)
         config = LoraConfig.from_pretrained(lora_config_path)
-        lora_weights = torch.load(lora_weights_path)
+        lora_weights = torch.load(lora_weights_path, map_location="cpu")
         model = PeftModel(model, config)
-        set_peft_model_state_dict(model,lora_weights,"default")
+        set_peft_model_state_dict(model, lora_weights, "default")
         del lora_weights
 
+    model.config.pad_token_id = tokenizer.pad_token_id
+    if tokenizer.bos_token_id is not None:
+        model.config.bos_token_id = tokenizer.bos_token_id
+    if tokenizer.eos_token_id is not None:
+        model.config.eos_token_id = tokenizer.eos_token_id
 
-    # unwind broken decapoda-research config
-    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
-    model.config.bos_token_id = 1
-    model.config.eos_token_id = 2
-
-    if not load_8bit:
-        model.half()  # seems to fix bugs for some users.
+    if not load_8bit and device != "cpu":
+        model.half()
 
     model.eval()
 
@@ -116,14 +131,21 @@ def main(
         prompt = prompter.generate_prompt(instruction, input)
         inputs = tokenizer(prompt, return_tensors="pt")
         input_ids = inputs["input_ids"].to(device)
+        attention_mask = inputs.get("attention_mask")
+        if attention_mask is not None:
+            attention_mask = attention_mask.to(device)
         generation_config = GenerationConfig(
             do_sample=False,
             num_beams=num_beams,
+            pad_token_id=tokenizer.pad_token_id,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
             **kwargs,
         )
 
         generate_params = {
             "input_ids": input_ids,
+            "attention_mask": attention_mask,
             "generation_config": generation_config,
             "return_dict_in_generate": True,
             "output_scores": True,
@@ -152,10 +174,9 @@ def main(
 
             with generate_with_streaming(**generate_params) as generator:
                 for output in generator:
-                    # new_tokens = len(output) - len(input_ids[0])
-                    decoded_output = tokenizer.decode(output)
+                    decoded_output = tokenizer.decode(output, skip_special_tokens=True)
 
-                    if output[-1] in [tokenizer.eos_token_id]:
+                    if tokenizer.eos_token_id is not None and output[-1] == tokenizer.eos_token_id:
                         break
 
                     yield prompter.get_response(decoded_output)
@@ -165,16 +186,17 @@ def main(
         with torch.no_grad():
             generation_output = model.generate(
                 input_ids=input_ids,
+                attention_mask=attention_mask,
                 generation_config=generation_config,
                 return_dict_in_generate=True,
                 output_scores=True,
                 max_new_tokens=max_new_tokens,
             )
         s = generation_output.sequences[0]
-        output = tokenizer.decode(s)
+        output = tokenizer.decode(s, skip_special_tokens=True)
         yield prompter.get_response(output)
 
-    sherpherd_UI=gr.Interface(
+    sherpherd_UI = gr.Interface(
         fn=evaluate,
         inputs=[
             gr.components.Textbox(
@@ -192,7 +214,7 @@ def main(
             gr.components.Checkbox(label="Stream output"),
         ],
         outputs=[
-            gr.inputs.Textbox(
+            gr.components.Textbox(
                 lines=5,
                 label="Output",
             )
@@ -201,7 +223,7 @@ def main(
         description="Shepherd is a LLM that has been fine-tuned in a federated manner ",
     ).queue()
 
-    sherpherd_UI.launch(share=True)
+    sherpherd_UI.launch(server_name=server_name, share=share_gradio)
 
 
 
